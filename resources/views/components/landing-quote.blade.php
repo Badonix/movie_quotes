cd788da6f09fe84494791c69672b2848ce25df4a
nick <nikoloz.danelia@redberry.ge>
2023-04-02 23:52:10 +0400

    refactor: href to named route

--- resources/views/components/landing-quote.blade.php
+++ resources/views/components/landing-quote.blade.php
@@ -1,5 +1,5 @@
 <div class='flex flex-col h-screen justify-center items-center gap-3'>
     <img src="{{$quote->image_url}}" class='w-5/12 max-w-5xl'/>
     <p class='text-white md:text-3xl lg:text-4xl mt-5 max-w-5xl text-center'>"{{$quote->body}}"</p>
-    <a href='/movie/{{$quote->movie->id}}' class='text-white mt-6 underline cursor-pointer'>{{$quote->movie->name}}</a>
+    <a href="{{ route('movie', ['id' => $quote->movie->id]) }}" class='text-white mt-6 underline cursor-pointer'>{{$quote->movie->name}}</a>
 </div>
