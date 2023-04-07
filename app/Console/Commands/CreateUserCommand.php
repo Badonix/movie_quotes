0a1936ab41e2d6e0348fad6d9c2566040b62113b
nick <nikoloz.danelia@redberry.ge>
2023-04-07 21:26:33 +0400

    feat: command to create user

---
+++ app/Console/Commands/CreateUserCommand.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\User;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Hash;
+
+class CreateUserCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'user:create {username} {email} {password}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Create user';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle(): void
+    {
+        $email = $this->argument('email');
+        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            $this->error('Invalid email address!');
+            return;
+        }
+
+        $username = $this->argument('username');
+        $password = $this->argument('password');
+
+        $user = new User();
+        $user->email = $email;
+        $user->username = $username;
+        $user->password = Hash::make($password);
+
+        try {
+            $user->save();
+            $this->info("User created!");
+        } catch (\Exception $e) {
+            $this->error("Error creating user: " . $e->getMessage());
+        }
+    }
+}
