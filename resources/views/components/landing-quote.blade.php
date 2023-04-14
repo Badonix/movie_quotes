477e625345e2734edbe5f5136df589de139f4b97
nick <nikoloz.danelia@redberry.ge>
2023-04-14 23:32:28 +0400

    fix: better size for images

--- resources/views/components/landing-quote.blade.php
+++ resources/views/components/landing-quote.blade.php
@@ -1,6 +1,6 @@
 @if($quote)
     <div class='flex flex-col h-screen justify-center items-center gap-3'>
-        <img src="{{asset('/storage/' . $quote->image_url)}}" class='w-5/12 max-w-5xl'/>
+        <img src="{{asset('/storage/' . $quote->image_url)}}" class='max-h-96 max-w-5xl object-cover'/>
         <p class='text-white md:text-3xl lg:text-4xl mt-5 max-w-5xl text-center'>"{{ $quote->getTranslation('body', app()->getLocale()) }}"</p>
         <a href="{{ route('movie', ['id' => $quote->movie->id]) }}" class='text-white mt-6 underline cursor-pointer text-2xl'>{{$quote->movie->name}}</a>
     </div>
