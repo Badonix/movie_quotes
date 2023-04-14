3461959d2545af153195ad1aaa94c1434ea92ac8
nick <nikoloz.danelia@redberry.ge>
2023-04-14 23:39:43 +0400

    fix: break-all for texts

--- resources/views/components/quote-row.blade.php
+++ resources/views/components/quote-row.blade.php
@@ -1,11 +1,11 @@
 <div class='bg-white rounded-xl px-5 py-3 flex items-center justify-between gap-5'>
     <p>{{$quote->id}}</p>
     <div class='flex text-sm items-center gap-2'>
-        <p class='text-center'>"{{ $quote->getTranslation('body', app()->getLocale()) }}"</p>
+        <p class='text-center break-all'>"{{ $quote->getTranslation('body', app()->getLocale()) }}"</p>
         <p>
         |
         </p>
-        <a href='{{route("movie", $quote->movie->id)}}'>{{$quote->movie->name}}</a>
+        <a href='{{route("movie", $quote->movie->id)}}' class='break-all'>{{$quote->movie->name}}</a>
     </div>
     <div class='flex items-center gap-3'>
         <a href='{{route("quote.edit", ["quote"=>$quote])}}'>{{__('setting.edit')}}</a>
--- resources/views/components/quote.blade.php
+++ resources/views/components/quote.blade.php
@@ -1,6 +1,6 @@
 <div class='w-full '>
     <img class='w-full rounded-t-md' src="{{asset('/storage/' . $quote->image_url)}}"/>
     <div class='bg-white py-5 px-2 rounded-b-md'>
-        <p>"{{$quote->body}}"</p>
+        <p class='break-all'>"{{$quote->body}}"</p>
     </div>
 </div>
