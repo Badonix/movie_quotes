2e81fc7cd5ffbc707e7ab5f52aab89af0d797a3e
nick <nikoloz.danelia@redberry.ge>
2023-04-15 18:39:23 +0400

    fix: inputs not changing position when error appears

--- resources/views/admin/movies/create.blade.php
+++ resources/views/admin/movies/create.blade.php
@@ -3,16 +3,18 @@
     <form action="{{route('movie.store')}}" method="POST" class='bg-white flex flex-col gap-5 rounded-xl px-5 py-3'>
         @csrf
         <div>
-        <x-input :value="old('name.ka')" name="name[ka]" form_name="movie_ka"/>
+        <x-input :value="old('name.ka')" name="name[ka]" form_name="movie_ka">
           @error('name.ka')
-          <p class='text-red-500 text-sm'>{{$message}}</p>
+          <p class='text-red-400 text-xs absolute -bottom-3'>{{$message}}</p>
           @enderror
+        </x-input>
         </div>
         <div>
-          <x-input :value="old('name.en')" name='name[en]' form_name='movie_en'/>
-          @error('name.en')
-          <p class='text-red-500 text-sm'>{{$message}}</p>
-          @enderror
+          <x-input :value="old('name.en')" name='name[en]' form_name='movie_en'>
+            @error('name.en')
+            <p class='text-red-400 text-xs absolute -bottom-3'>{{$message}}</p>
+            @enderror
+          </x-input>
         </div>
         <button class='border p-1 mt-3 hover:bg-slate-100' type='submit'>{{__('setting.add')}}</button>
     </form>
--- resources/views/admin/quotes/create.blade.php
+++ resources/views/admin/quotes/create.blade.php
@@ -3,22 +3,25 @@
     <form method="POST" action="{{route('quote.store')}}" enctype="multipart/form-data" class='bg-white flex flex-col gap-5 rounded-xl px-5 py-3'>
         @csrf
         <div>
-          <x-input :value="old('body.en')" name='body[en]' form_name='quote_en'/>
-          @error("body.en")
-          <p class='text-red-400 text-sm'>{{$message}}</p>
-          @enderror
+          <x-input :value="old('body.en')" name='body[en]' form_name='quote_en'>
+            @error("body.en")
+            <p class='text-red-400 text-xs absolute -bottom-3'>{{$message}}</p>
+            @enderror
+          </x-input>
         </div>
         <div>
-          <x-input :value="old('body.ka')" name='body[ka]' form_name='quote_ka'/>
-          @error("body.ka")
-          <p class='text-red-400 text-sm'>{{$message}}</p>
-          @enderror
+          <x-input :value="old('body.ka')" name='body[ka]' form_name='quote_ka'>
+            @error("body.ka")
+            <p class='text-red-400 text-xs absolute -bottom-3'>{{$message}}</p>
+            @enderror
+          </x-input>
         </div>
         <div>
-          <x-input form_name='image' name='image_url' type='file'/>
-          @error('image_url')
-          <p class='text-red-400 text-sm'>{{$message}}</p>
-          @enderror
+          <x-input form_name='image' name='image_url' type='file'>
+            @error('image_url')
+            <p class='text-red-400 text-xs absolute -bottom-3'>{{$message}}</p>
+            @enderror
+          </x-input>
         </div>
         <div class='flex flex-col gap-1'>
             <label for='movie_id'>{{__('labels.movie')}}</label>
@@ -28,7 +31,7 @@
               @endforeach
             </select>
             @error('movie_required')
-              <p class='text-red-400 text-sm'>{{$message}}</p>
+              <p class='text-red-400 text-xs'>{{$message}}</p>
             @enderror
         </div>
         <button class='border p-1 mt-3 hover:bg-slate-100' type='submit'>{{__('setting.add')}}</button>
